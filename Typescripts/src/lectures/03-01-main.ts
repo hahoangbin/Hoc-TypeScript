354de246e32f0095ae8bc922b98686a1e47dc02f
Ha Ngoc Duong <hahoangbin999@gmail.com>
2023-04-15 22:02:58 +0700

    Add Bai-14-15 Type Alias and Interface

--- Typescripts/src/lectures/03-01-main.ts
+++ Typescripts/src/lectures/03-01-main.ts
@@ -37,3 +37,66 @@ const e: Employee = {
   phone: 12345,
   credit: 100001,
 }
+
+type NewType = number & string // never
+
+function fail(message: string): never {
+  throw new Error(message)
+}
+
+function fn(x: string | number) {
+  if ( typeof x === "string") {
+    // do something
+  } else if ( typeof x == "number") {
+    // do something else
+  } else {
+    x // has type 'never'
+  }
+}
+
+// cach 1
+function createStudent(student: {id: number, name: string, age: number})
+{
+  const { id, name, age } = student
+  console.log(id, name, age);
+}
+createStudent ({
+  id: 1,
+  name: 'aaa',
+  age: 18
+})
+
+// cach 2
+function createStd2({id, name, age}: {id: number, name: string, age: number})
+{
+  console.log(id, name, age);
+}
+createStd2 ({
+  id: 1,
+  name: 'aaa',
+  age: 18
+})
+
+// cach 3
+interface create3 {
+  id: number,
+  name: string,
+  age: number
+}
+function createname3 ({id, name, age}: create3) {
+  console.log(id, name, age);
+}
+createname3 ({
+  id: 1,
+  name: 'aaa',
+  age: 18
+})
+
+// bang type compatible
+// 1. string, number, boolean
+// 2. null
+// 3. undefined
+// 4. any
+// 5. unknown
+// 6. void
+// 7. never
